5cfb84b0d9cd2b9278cb2520b47f93d2f7f63968
Mujtaba <gmabbasi92@gmail.com>
2023-04-08 06:20:44 +0500

    s

---
+++ Table/Tabke.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const Table = () => {
+    return (<>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+        <div>Table</div>
+    </>
+
+    )
+}
+
+export default Table
